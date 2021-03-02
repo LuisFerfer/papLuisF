0bb098aab8b266ea836d270ed6607c0a21180c4d
Luis André <luis_dede@sapo.pt>
2021-03-02 12:31:30 +0000

    layout duelo

---
+++ duelo2.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<script src="js/jquery.min.js"></script>
+<script src="js/common.js"></script>
+<script>
+
+
+</script>
+<div class="row">
+<div class="col-6">
+    <div class="centered">
+        <h1 style="color: white">Jogador 1</h1>
+        <p>timer</p>
+        <p style="color: palegreen">i love grillers</p>
+        <div style="padding-top: 60px">
+            <a  class="button" id="novo">NOVO</a>
+            <a  class="button" id="velho">VISTO</a>
+        </div>
+    </div>
+</div>
+
+<div class="col-6">
+    <div class="centered">
+        <h1 style="color: white">Jogador 2</h1>
+        <p>timer </p>
+        <p style="color: orange">they like big monky</p>
+        <div style="padding-top: 60px">
+            <a  class="button" id="novo">NOVO</a>
+            <a  class="button" id="velho">VISTO</a>
+        </div>
+    </div>
+</div>
+</div>
