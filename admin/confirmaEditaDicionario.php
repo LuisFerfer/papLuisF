d41b6a2f62038d10c9714f2a4f95a8ff62f072c4
Luis André <luis_dede@sapo.pt>
2021-03-22 13:50:52 +0000

    edit dicio no work

--- admin/confirmaEditaDicionario.php
+++ admin/confirmaEditaDicionario.php
@@ -3,15 +3,18 @@ $con=mysqli_connect("localhost","root","","pap2021vermo");
 $nome=addslashes($_POST['nomeDicionario']);
 $id=intval($_POST['id']);
 $ficheiro=($_FILES['dicionarioFicheiroURL']['name']);
+$nivel=addslashes($_POST['dicionarioNivel']);
+$linguaid=intval($_POST['dicionarioLinguaId']);
 
 
-$sql="Update dicionarios set dicionarioNome='".$nome."'";
+
+$sql="Update dicionarios set dicionarioNome='".$nome."',dicionarioNivel='".$nivel."',dicionarioLinguaId='".linguaid."',dicionarioFicheiroURL='".$ficheiro."'";
 if($ficheiro!=''){
-    $sql.=", dicionarioFicheiroURL='".$ficheiro."'";
+    $sql.="dicionarioFicheiroURL='".$ficheiro."'";
 
 }
 
-$sql.=" where dicionarioId=".$id;
+
 
 
 mysqli_query($con,$sql);
