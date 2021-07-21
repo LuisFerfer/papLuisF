79ffc796a11f04d0c917c26b4e76e1be2f91eab7
LuisFerfer <undeaddede12@gmail.com>
2021-07-21 20:05:36 +0100

    site terminado

--- pap2021vermo.sql
+++ pap2021vermo.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost_3306
+ Source Server         : pap2021vermo
  Source Server Type    : MySQL
  Source Server Version : 50731
  Source Host           : localhost:3306
@@ -11,7 +11,7 @@
  Target Server Version : 50731
  File Encoding         : 65001
 
- Date: 21/07/2021 16:59:01
+ Date: 21/07/2021 20:05:21
 */
 
 SET NAMES utf8mb4;
@@ -30,7 +30,7 @@ CREATE TABLE `dicionarios`  (
   PRIMARY KEY (`dicionarioId`) USING BTREE,
   INDEX `fk_dicionarios_linguas_idx`(`dicionarioLinguaId`) USING BTREE,
   CONSTRAINT `fk_dicionarios_linguas` FOREIGN KEY (`dicionarioLinguaId`) REFERENCES `linguas` (`linguaId`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of dicionarios
@@ -51,7 +51,7 @@ CREATE TABLE `jogos`  (
   INDEX `fk_jogos_dicionarios1_idx`(`jogoDicionarioId`) USING BTREE,
   CONSTRAINT `fk_jogos_dicionarios1` FOREIGN KEY (`jogoDicionarioId`) REFERENCES `dicionarios` (`dicionarioId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_jogos_utilizadores1` FOREIGN KEY (`jogoUtilizadorId`) REFERENCES `utilizadores` (`utilizadorId`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of jogos
@@ -66,7 +66,7 @@ CREATE TABLE `linguas`  (
   `linguaNome` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `linguaBandeiraURL` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`linguaId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of linguas
@@ -87,13 +87,16 @@ CREATE TABLE `perfis`  (
   PRIMARY KEY (`perfilId`) USING BTREE,
   INDEX `fk_perfis_utilizadores1_idx`(`perfilId`) USING BTREE,
   CONSTRAINT `fk_perfis_utilizadores1` FOREIGN KEY (`perfilId`) REFERENCES `utilizadores` (`utilizadorId`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of perfis
 -- ----------------------------
 INSERT INTO `perfis` VALUES (8, 'luis', 'images/boss.png', 'images/pic03.jpg', 'luis@email.com');
 INSERT INTO `perfis` VALUES (42, 'Leandro', 'images/noAvatar.jpg', 'images/h.jpg', 'luis_dede@sapo.pt');
+INSERT INTO `perfis` VALUES (43, 'P3dr0', 'images/noAvatar.jpg', 'images/h.jpg', 'pedrox@gmail.com');
+INSERT INTO `perfis` VALUES (44, 'JoÃ£oGamer', 'images/noAvatar.jpg', 'images/h.jpg', 'jotag@email.pt');
+INSERT INTO `perfis` VALUES (45, 'Simao', 'images/noAvatar.jpg', 'images/h.jpg', 'simao@gmail.com');
 
 -- ----------------------------
 -- Table structure for tops
@@ -106,11 +109,23 @@ CREATE TABLE `tops`  (
   PRIMARY KEY (`topId`) USING BTREE,
   INDEX `fk_tops_utilizadores1_idx`(`topUtilizadorId`) USING BTREE,
   CONSTRAINT `fk_tops_utilizadores1` FOREIGN KEY (`topUtilizadorId`) REFERENCES `utilizadores` (`utilizadorId`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 36 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of tops
 -- ----------------------------
+INSERT INTO `tops` VALUES (24, 12, 8);
+INSERT INTO `tops` VALUES (25, 9, 8);
+INSERT INTO `tops` VALUES (26, 22, 8);
+INSERT INTO `tops` VALUES (27, 18, 8);
+INSERT INTO `tops` VALUES (28, 16, 8);
+INSERT INTO `tops` VALUES (29, 17, 42);
+INSERT INTO `tops` VALUES (30, 13, 43);
+INSERT INTO `tops` VALUES (31, 22, 44);
+INSERT INTO `tops` VALUES (32, 34, 44);
+INSERT INTO `tops` VALUES (33, 19, 43);
+INSERT INTO `tops` VALUES (34, 0, 8);
+INSERT INTO `tops` VALUES (35, 0, 8);
 
 -- ----------------------------
 -- Table structure for utilizadores
@@ -124,7 +139,7 @@ CREATE TABLE `utilizadores`  (
   `utilizadorPassword` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`utilizadorId`) USING BTREE,
   UNIQUE INDEX `utilizadorLogin_UNIQUE`(`utilizadorLogin`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 43 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 46 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of utilizadores
@@ -132,5 +147,8 @@ CREATE TABLE `utilizadores`  (
 INSERT INTO `utilizadores` VALUES (8, 'luis', 'ativo', 'admin', '502ff82f7f1f8218dd41201fe4353687');
 INSERT INTO `utilizadores` VALUES (10, 'pedro', 'ativo', 'utilizador', '12345');
 INSERT INTO `utilizadores` VALUES (42, 'luis_dede@sapo.pt', 'ativo', 'utilizador', '827ccb0eea8a706c4c34a16891f84e7b');
+INSERT INTO `utilizadores` VALUES (43, 'pedrox@gmail.com', 'ativo', 'utilizador', '827ccb0eea8a706c4c34a16891f84e7b');
+INSERT INTO `utilizadores` VALUES (44, 'jotag@email.pt', 'ativo', 'utilizador', '827ccb0eea8a706c4c34a16891f84e7b');
+INSERT INTO `utilizadores` VALUES (45, 'simao@gmail.com', 'ativo', 'utilizador', '827ccb0eea8a706c4c34a16891f84e7b');
 
 SET FOREIGN_KEY_CHECKS = 1;
